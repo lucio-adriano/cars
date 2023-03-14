43a8218192cbb89bb08878f02215c93ee09a1117
Adriano Lucio <lucio.adriano@gmail.com>
2023-03-14 16:58:29 -0300

    Configurando a estrutura do java web

---
+++ src/main/java/br/com/silva/cars/TesteServlet.java
@@ -0,0 +1,23 @@
+package br.com.silva.cars;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/testeservlet")
+public class TesteServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	System.out.println("Teste Servelet");
+	
+	RequestDispatcher reqDispatcher = req.getRequestDispatcher("cars.jsp");
+	reqDispatcher.forward(req, resp);
+	
+	}
+
+}
